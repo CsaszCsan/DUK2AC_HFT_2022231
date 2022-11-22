6041e29fd7ebc534b7f2f5e15b3d440c0ae94d18
CsaszCsan <116065656+CsaszCsan@users.noreply.github.com>
2022-11-22 13:31:32 +0100

    FrontEnd Created

---
+++ DUK2AC_HFT_2022231/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DUK2AC_HFT_2022231
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
