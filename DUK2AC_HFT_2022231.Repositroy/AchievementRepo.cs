8d1a4e7ddc116c4d17d2be768330b319ef6ca149
CsaszCsan <116065656+CsaszCsan@users.noreply.github.com>
2022-12-01 19:00:26 +0100

    Repos Done

---
+++ DUK2AC_HFT_2022231.Repositroy/AchievementRepo.cs
@@ -0,0 +1,17 @@
+﻿using DUK2AC_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DUK2AC_HFT_2022231.Repositroy
+{
+    class AchievementRepo:Repo<Achievement>,IRepo<Achievement>
+    {
+        public AchievementRepo(GameShopDbContext gctx) : base(gctx)
+        {
+
+        }
+    }
+}
---
+++ DUK2AC_HFT_2022231.Repositroy/GameRepo.cs
@@ -0,0 +1,19 @@
+﻿using DUK2AC_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DUK2AC_HFT_2022231.Repositroy
+{
+    class GameRepo : Repo<Game>, IRepo<Game>
+    {
+        public GameRepo(GameShopDbContext gctx) : base(gctx)
+        {
+
+        }
+
+       
+    }
+}
