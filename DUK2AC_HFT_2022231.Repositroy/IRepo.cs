3c632c18479b7c3cb20bac02a430cce9cc5e38e8
CsaszCsan <116065656+CsaszCsan@users.noreply.github.com>
2022-12-01 18:39:57 +0100

    Repo base classes done

---
+++ DUK2AC_HFT_2022231.Repositroy/IRepo.cs
@@ -0,0 +1,24 @@
+﻿using DUK2AC_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DUK2AC_HFT_2022231.Repositroy
+{
+    interface IRepo<T> where T : DataItem
+    {
+        
+          IQueryable<T> ReadAll();
+
+          void Create(T item);
+
+          T Read(int ID);
+
+          void Update(T item);
+
+          void Delete(int ID);
+        
+    }
+}
---
+++ DUK2AC_HFT_2022231.Repositroy/Repo.cs
@@ -0,0 +1,40 @@
+﻿using DUK2AC_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DUK2AC_HFT_2022231.Repositroy
+{
+    abstract class Repo<T>:IRepo<T> where T:DataItem
+    {
+        protected GameShopDbContext gctx;
+
+        public Repo(GameShopDbContext context)
+        {
+            this.gctx = context;
+        }
+
+        public void Create(T item)
+        {
+            gctx.Set<T>().Add(item);
+            gctx.SaveChanges();
+        }
+
+        public abstract T Read(int ID);
+
+        public abstract void Update(T item);
+
+        public void Delete(int ID)
+        {
+            gctx.Set<T>().Remove(Read(ID));
+            gctx.SaveChanges();
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return gctx.Set<T>();
+        }
+    }
+}
